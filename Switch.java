a3b2d2861f7a270c3dd1da57a6e5a4166ff6606c
goswamiakshay <44131661+goswamiakshay@users.noreply.github.com>
2023-07-14 18:16:57 +0530

    Add files via upload

--- Switch.java
+++ Switch.java
@@ -1,86 +1,145 @@
+package Akshay;
+
 import java.util.Scanner;
 
 public class Switch {
+
 	public static void main(String[] args) {
-		int choice, n1,n2,n3,n4,n5,p,n,r,result;
-		char input;
-		Scanner sc= new Scanner(System.in);
-		System.out.println("1: Even ODD\n2:Greater between 5no\n3:Vowels or not\n4:Rate of interest");
-		choice=sc.nextInt();
-		switch(choice)
-		{
-		case 1:
-			System.out.println("Enter a number");
-			n1=sc.nextInt();
-			if(n1%2==0)
+//		initializing variables
+		int choice, num1, num2;
+		char repeat;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("1: Arithmetic operators \n2: Assingment operators \n3: Bitwise operator \n4: Logical  operator \n5: Relational Operator \n6: Ternary operator");
+		
+		
+		
+//		do while for repetition 
+		do {
+
+//			ask user to which operation run
+			System.out.println("\nEnter the number which operator you want to perform");
+			choice = sc.nextInt();
+		
+	
+			switch(choice) 
 			{
-				System.out.println("EVEN "+n1);
-			}
-			else
-			{
-				System.out.println("ODD "+ n1);
-			}
-			break;
-		case 2:
-			System.out.println("Enter 5 Number : ");
-			n1=sc.nextInt();
-			n2=sc.nextInt();
-			n3=sc.nextInt();
-			n4=sc.nextInt();
-			n5=sc.nextInt();
-			if(n1>n2&&n1>n3&&n1>n4&&n1>n5)
-			{
-				System.out.println("Number1 is greater " + n1);
+			
+	//		for arithmetic operator
+				case 1:{
+					System.out.println("Your choice is Arithmetic operators");
+					System.out.println("\nEnter the any two number");
+					num1 = sc.nextInt();
+					num2 = sc.nextInt();
+					int add = num1 + num2;
+					int sub = num1 - num2;
+					int div = num1 / num2;
+					int mul = num1 * num2;
+					int mod = num1 % num2;
+					System.out.println("Addition is: "+ add);
+					System.out.println("Subtraction is: "+ sub);
+					System.out.println("Division is: "+ div);
+					System.out.println("Multiplication is: "+ mul);
+					System.out.println("Modulus is: "+ mod);
+					break;
+					}
 				
-			}
-			else if(n2>n1&&n2>n3&&n2>n4&&n2>n5)
-			{
-				System.out.println("Number2 is greater " + n2);
-			}
-			else if(n3>n1&&n3>n2&&n3>n4&&n3>n5)
-			{
-				System.out.println("Number3 is greater " + n3);
-			}
-			else if(n4>n1&&n4>n3&&n4>n3&&n4>n5)
-			{
-				System.out.println("Number4 is greater " + n4);
-			}
-			else
-			{
-				System.out.println("Number5 is greater " + n5);
-			}
-		case 3:
-			System.out.println("Enter any character");
-			input=sc.next().charAt(0);
-			switch(input)
-			{
-			case 'a':
-			case 'e':
-			case 'i':
-			case 'o':
-			case 'u':
 				
-			System.out.println(input+"is a vowel");
-			break;
-			default:
-			System.out.println(input+"is not a vowel");
-			break;
+	//			Assignment operator
+				case 2:{
+					System.out.println("Your choice is Assingment operators");
+					System.out.println("\nEnter the single number");
+					num1 = sc.nextInt();
+					
+					
+					System.out.println("num1+=10 : "+(num1+=10));
+					System.out.println("num1-=10 : "+(num1-=10));
+					System.out.println("num1*=10 : "+(num1*=10));
+					System.out.println("num1/=10 : " +(num1/=10));
+					System.out.println("num1%=10 : " + (num1%=10));
+					System.out.println("num1=10 : "+(num1=10));
+					
+					break;
+					}
+				
+				
+	//			for bitwise operator
+				case 3:{
+					System.out.println("Your choice is Bitwise operators");
+					System.out.println("\nEnter the any two number");
+					num1 = sc.nextInt();
+					num2 = sc.nextInt();
+					System.out.println("(num1>num2)&(num1<num2)"+((num1>num2)&(num1<num2)));
+					System.out.println("(num1>num2)&(num1<num2)"+((num1>num2)^(num1<num2)));
+					System.out.println("(num1>num2)&(num1<num2)"+((num1>num2)|(num1<num2)));
+					break;
+					}
+				
+	//			for Logical operator
+				case 4:{
+					System.out.println("Your choice is Logical operators");
+					System.out.println("\nEnter the any two number");
+					num1 = sc.nextInt();
+					num2 = sc.nextInt();
+					System.out.println("(num1 > num2) && (num1 < num2)" +((num1 > num2) && (num1 < num2)) );
+					System.out.println("(num1 > num2) || (num1 < num2)" +((num1 > num2) || (num1 < num2)) );
+					System.out.println("!(num1 > num2)"+(!(num1 > num2)) );
+					
+					break;
+					}
+				
+	//			for relational operator
+				case 5:{
+					System.out.println("Your choice is Relational operators");
+					System.out.println("\nEnter the any two number");
+					num1 = sc.nextInt();
+					num2 = sc.nextInt();
+					System.out.println("for check num1 is greater than num2: "+ (num1 > num2));
+					System.out.println("for check num1 is lesser than num2: "+ (num1 < num2));
+					System.out.println("for check num1 is equal to num2: "+ (num1 == num2));
+					System.out.println("for check num1 is less than equal to num2: "+ (num1 <= num2));
+					System.out.println("for check num1 is greater than equal to num2: "+ (num1 >= num2));
+					System.out.println("for check num1 is not equal to num2: "+ (num1 != num2));
+					
+					break;
+					}
+				
+				
+	//	for ternary operator
+				case 6:{
+					System.out.println("Your choice is Ternary operators");
+					System.out.println("\nEnter the any two number");
+					num1 = sc.nextInt();
+					num2 = sc.nextInt();
+					
+					String res=(num1 < num2) ? "Num 2 is greater than Num 1 ": "Num 2 is lesser than Num 1";	
+					System.out.println(res);
+					
+					break;
+					}
+				
+				default:{
+					System.out.println("invalid inpuit");
+				}
 			}
-			break;
+				
+
 			
-		case 4:
-			System.out.println("Enter P, R, N");
-			
-			p=sc.nextInt();
-			r=sc.nextInt();
-			n=sc.nextInt();
-			result=(p*n*r)/100;
-			System.out.println("Rate of Interest is ="+result);
-			break;
-			default:
-				System.out.println("Invalid choice");
-				break;
-		}
-			
+//			ask user for repetition
+				System.out.println("\n\nDo you want to perform another operation...(y / n):");
+			 repeat = sc.next().charAt(0);
+				
+				
+				
+		}while(repeat == 'Y' || repeat == 'y');
+		
+		
+//		final message
+		System.out.println("\n\nThank you... END");
+		
+	
+		
 	}
+
+
 }
